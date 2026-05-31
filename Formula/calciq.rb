b196c3d664f2307c0a177045742e720b2486a20d
Sarbmeet Kandhari <sarab.k23@gmail.com>
2026-05-31 08:15:56 -0600

    calciq 1.0.1

--- Formula/calciq.rb
+++ Formula/calciq.rb
@@ -2,9 +2,9 @@
 class Calciq < Formula
   desc "Terminal-native calculator (calciq CLI)"
   homepage "https://www.simplemind.dev/calciq"
-  version "1.0.0"
+  version "1.0.1"
   url "https://github.com/sqlaide/homebrew-calciq-releases/releases/download/v#{version}/calciq-macos-arm64.tar.gz"
-  sha256 "c126210ae23f1b82b5e1307891118d02c946fdde273368aea63c524840734eab"
+  sha256 "01eabe2d2567755680728b537bbd45a96902187d69cd9eef1abc9fed84cdcf6d"
 
   depends_on :macos
 
